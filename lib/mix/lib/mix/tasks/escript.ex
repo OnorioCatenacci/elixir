88991da3245bf3530e9da6c7e521bea265d98c59
Alexei Sholik <alcosholik@gmail.com>
2014-12-28 23:50:11 +0200

    Implement 'mix escript' task

--- lib/mix/lib/mix/tasks/escript.ex
+++ lib/mix/lib/mix/tasks/escript.ex
@@ -14,12 +14,22 @@ defmodule Mix.Tasks.Escript do
 
   @spec run(OptionParser.argv) :: :ok
   def run(_) do
-    escripts_path = Mix.Local.path_for(:escript)
+    escripts_path = Mix.Local.escripts_path
+
     escripts =
       escripts_path
-      |> list_dir()
-      |> Enum.filter(fn filename -> executable?(Path.join(escripts_path, filename)) end)
-    Mix.Local.Installer.print_list(:escript, escripts)
+      |> list_dir
+      |> Enum.filter(fn filename -> executable?(Path.join([escripts_path, filename])) end)
+
+    if escripts == [] do
+      Mix.shell.info "No escripts currently installed."
+    else
+      Enum.each escripts, fn filename ->
+        Mix.shell.info "* #{filename}"
+      end
+
+      Mix.shell.info "Escripts installed at: #{escripts_path}"
+    end
   end
 
   defp list_dir(path) do
@@ -33,10 +43,6 @@ defmodule Mix.Tasks.Escript do
     owner_exec_bit = 0o00100
     group_exec_bit = 0o00010
     other_exec_bit = 0o00001
-    stat = File.stat!(path)
-
-    executable_bit =
-      stat.mode &&& (owner_exec_bit ||| group_exec_bit ||| other_exec_bit)
-    executable_bit != 0 and stat.type == :regular and Path.extname(path) != ".bat"
+    (File.stat!(path).mode &&& (owner_exec_bit ||| group_exec_bit ||| other_exec_bit)) != 0
   end
 end
